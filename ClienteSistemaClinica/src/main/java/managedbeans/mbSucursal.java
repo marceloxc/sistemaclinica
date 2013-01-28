dbef76ba686481b35cb3bff9aa59568bcae487d0
tnoduse <tnoduse@gmail.com>
2013-01-27 20:24:31 -0500

    Revert "agregado propiedad mbSucursal"
    This reverts commit 80b724ee91ca022807aee255ed41305d58548056.

--- ClienteSistemaClinica/src/main/java/managedbeans/mbSucursal.java
+++ ClienteSistemaClinica/src/main/java/managedbeans/mbSucursal.java
@@ -21,7 +21,6 @@ public class mbSucursal implements Serializable{
     private SucursalWS_Service service;
     private int dato;
     private String nombre;
-    private String direccion;
 
     public String getNombre() {
         return nombre;
@@ -70,7 +69,7 @@ public class mbSucursal implements Serializable{
         try { // Call Web Service Operation
             ws.SucursalWS port = service.getSucursalWSPort();
             // TODO initialize WS operation arguments here
-            int id = dato;
+            java.lang.String id = dato+"";
             // TODO process result her
             ws.Sucursal result = port.finds(id);
             nombre=result.getDireccion();
@@ -81,13 +80,4 @@ public class mbSucursal implements Serializable{
 
         
     }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-    
 }
