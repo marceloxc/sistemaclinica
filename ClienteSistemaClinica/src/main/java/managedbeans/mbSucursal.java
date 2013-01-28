6faed05de1c560a62b1c2d6f79dc2c09550d2682
Valex <Valex@Valex-PC>
2013-01-28 01:33:28 -0500

    Cambios Sucursal

--- ClienteSistemaClinica/src/main/java/managedbeans/mbSucursal.java
+++ ClienteSistemaClinica/src/main/java/managedbeans/mbSucursal.java
@@ -1,14 +1,19 @@
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package managedbeans;
 
+
 import java.io.Serializable;
+import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.xml.ws.WebServiceRef;
-import org.primefaces.model.LazyDataModel;
+import org.primefaces.event.RowEditEvent;
 import ws.Sucursal;
 import ws.SucursalWS_Service;
 
@@ -18,68 +23,31 @@ import ws.SucursalWS_Service;
  */
 @ManagedBean
 @SessionScoped
-public class mbSucursal implements Serializable{
+public final class mbSucursal implements Serializable{
     @WebServiceRef(wsdlLocation = "WEB-INF/wsdl/localhost_8080/ServidorSistemaClinica/SucursalWS.wsdl")
     private SucursalWS_Service service;
     private int dato;
+    private String nombre;
     private String direccion;
-    private String telefono;
-    private LazyDataModel<Sucursal> lazySucursal;  
-    private Sucursal selectSucursal; 
+    private List<Sucursal> listado;
 
-    public LazyDataModel<Sucursal> getLazySucursal() {
-        return lazySucursal;
+    public List<Sucursal> getListado() {
+        return listado;
     }
 
-    public void setLazySucursal(LazyDataModel<Sucursal> lazySucursal) {
-        this.lazySucursal = lazySucursal;
-    }
-
-    public Sucursal getSelectSucursal() {
-        return selectSucursal;
-    }
-
-    public void setSelectSucursal(Sucursal selectSucursal) {
-        this.selectSucursal = selectSucursal;
+    public void setListado(List<Sucursal> listado) {
+        this.listado = listado;
     }
 
     
-    /**
-     * Get the value of telefono
-     *
-     * @return the value of telefono
-     */
-    public String getTelefono() {
-        return telefono;
+    public String getNombre() {
+        return nombre;
     }
 
-    /**
-     * Set the value of telefono
-     *
-     * @param telefono new value of telefono
-     */
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
-
-    /**
-     * Get the value of direccion
-     *
-     * @return the value of direccion
-     */
-    public String getDireccion() {
-        return direccion;
-    }
-
-    /**
-     * Set the value of direccion
-     *
-     * @param direccion new value of direccion
-     */
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
+    
     public int getDato() {
         return dato;
     }
@@ -93,6 +61,7 @@ public class mbSucursal implements Serializable{
      * Creates a new instance of mbSucursal
      */
     public mbSucursal() {
+        //Listar();
     }
     
     public String crear() {
@@ -101,12 +70,14 @@ public class mbSucursal implements Serializable{
             ws.SucursalWS port = service.getSucursalWSPort();
             // TODO initialize WS operation arguments here
             ws.Sucursal sucursal = new ws.Sucursal();
+            //sucursal.setNumSucursal(dato);
             sucursal.setDireccion(direccion);
-            sucursal.setTelefono(telefono);
+            sucursal.setTelefono(nombre);
       //      sucursal.setTelefono('0999855');
             // TODO process result here
             boolean result = port.crears(sucursal);
-            System.out.println("Result = "+result);       
+            System.out.println("Result = "+result);
+            return "index";
         } catch (Exception ex) {
         // TODO handle custom exceptions here
         }
@@ -123,6 +94,7 @@ public class mbSucursal implements Serializable{
             // TODO process result her
             ws.Sucursal result = port.finds(id);
             direccion=result.getDireccion();
+            nombre=result.getTelefono();
             System.out.println("Result = "+result);
         } catch (Exception ex) {
         // TODO handle custom exceptions here
@@ -130,4 +102,58 @@ public class mbSucursal implements Serializable{
 
         
     }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    public void onEdit(RowEditEvent event) {  
+        FacesMessage msg = new FacesMessage("Sucursal Editada", ((Sucursal) event.getObject()).getNumSucursal()+"");    
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        
+        try { // Call Web Service Operation
+            ws.SucursalWS port = service.getSucursalWSPort();
+            // TODO initialize WS operation arguments here
+            ws.Sucursal sucursal = (Sucursal)event.getObject();
+            // TODO process result here
+            direccion = sucursal.getDireccion();
+            nombre =sucursal.getTelefono();
+            boolean result = port.edits(sucursal);
+            System.out.println("Result = "+result);
+        } catch (Exception ex) {
+        // TODO handle custom exceptions here
+        }
+
+        
+        
+    }  
+    
+    public void onCancel(RowEditEvent event) {  
+        FacesMessage msg = new FacesMessage("Car Cancelled", ((Sucursal) event.getObject()).toString());  
+  
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }
+    
+    public void Listar(){
+        
+        try { // Call Web Service Operation
+           int fi = 0;
+            int max = 2;
+            ws.SucursalWS port = service.getSucursalWSPort();
+            // TODO initialize WS operation arguments here
+            
+            // TODO process result here
+            java.util.List<ws.Sucursal> result = port.findentitiess(fi, max);
+            listado = result;
+            System.out.println("Result = "+result);
+        } catch (Exception ex) {
+        // TODO handle custom exceptions here
+        }
+
+    }
 }
+
