d32bc189f8d5e21adca0138b699fb1de375acb29
Fernanda <Fernanda@Fernanda>
2013-01-29 10:24:57 -0500

    entidades correctas

---
+++ ClienteSistemaClinica/src/main/java/managedbeans/mbDetalleCartera.java
@@ -0,0 +1,58 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedbeans;
+
+import java.util.Date;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Fernanda
+ */
+@ManagedBean
+@RequestScoped
+public class mbDetalleCartera {
+
+    /**
+     * Creates a new instance of mbDetalleCartera
+     */
+    String fecha;
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getAbono() {
+        return abono;
+    }
+
+    public void setAbono(double abono) {
+        this.abono = abono;
+    }
+
+    public int getIdCartera() {
+        return idCartera;
+    }
+
+    public void setIdCartera(int idCartera) {
+        this.idCartera = idCartera;
+    }
+    double abono;
+    int idCartera;
+    
+    public mbDetalleCartera() {
+        java.util.Date fecha2 = new Date();;
+        fecha=fecha2.getDate()+"-"+(Integer.parseInt(fecha2.getMonth()+"")+1)+"-"+(Integer.parseInt(fecha2.getYear()+"")+1900);
+        
+
+    }
+    
+    
+}
